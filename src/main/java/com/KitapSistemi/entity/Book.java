fb5bd354c5e85fdcf26251f68e3a760c5eb78e48
Abdullah Anli <anliabdullah08@gmail.com>
2023-11-16 14:50:28 +0300

    Model ve Servisler eklendi

---
+++ src/main/java/com/KitapSistemi/entity/Book.java
@@ -0,0 +1,21 @@
+package com.KitapSistemi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private int publicationYear;
+
+}
---
+++ src/test/java/com/KitapSistemi/KitapSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KitapSistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitapSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
